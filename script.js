0fdc5f1624e16d8c0c26ebb389d43a7e8b655c26
Shreevallabh Tripathi <157093445+Vallabh0440@users.noreply.github.com>
2025-01-20 15:19:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.box');
+
+buttons.forEach(function (button){
+    console.log(button);
+    button.addEventListener('click',function(event){
+        console.log(event);
+        console.log(event.target);
+        
+        if(event.target.id === 'grey') {
+            body.style.backgroundColor = 'grey'   
+        }
+        if(event.target.id === 'white') {
+            body.style.backgroundColor = 'white'   
+        }
+        if(event.target.id === 'blue') {
+            body.style.backgroundColor = 'blue'   
+        }
+        if(event.target.id === 'yellow') {
+            body.style.backgroundColor = 'yellow'   
+        }
+
+    })
+})
